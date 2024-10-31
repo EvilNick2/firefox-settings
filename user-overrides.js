8fe54f4c43647957dcab73413a655d923971004c
EvilNick2 <72648332+EvilNick2@users.noreply.github.com>
2024-10-31 10:06:05 +0000

    Updated to use user-overrides.js instead of directly editing user.js

---
+++ user-overrides.js
@@ -0,0 +1,5 @@
+// My overrides
+
+user_pref("browser.startup.page", 3); // Resume previous session on browser launch
+
+user_pref("privacy.sanitize.sanitizeOnShutdown", false); // Disable browser sanitization on shutdown
