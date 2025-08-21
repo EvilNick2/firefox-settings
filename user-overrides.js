b85af9c01e647f0bd0ecbdfdcc04f74874f54148
EvilNick2 <72648332+EvilNick2@users.noreply.github.com>
2025-08-21 10:29:02 +0100

    Update user.js overrides

--- user-overrides.js
+++ user-overrides.js
@@ -3,3 +3,7 @@
 user_pref("browser.startup.page", 3); // Resume previous session on browser launch
 
 user_pref("privacy.sanitize.sanitizeOnShutdown", false); // Disable browser sanitization on shutdown
+
+// Enable url bar and search live suggestion
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
